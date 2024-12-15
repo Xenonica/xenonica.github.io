9f5e2eb78b1cd25fc91ee9f9edce963acfcff31f
Xenonica <59960782+Xenonica@users.noreply.github.com>
2024-12-15 12:05:33 +0800

    optimization for seo, performance
    - added meta tag
    - remove unused scripts

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-// Make navbar stick to top of the page when scrolling
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $(".navibar");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
